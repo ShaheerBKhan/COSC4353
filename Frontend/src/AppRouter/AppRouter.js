b47a0ea21b24e11eab14da6d1700c5b7981d5751
Shaheer Khan <K.ShaheerB@gmail.com>
2022-02-15 10:56:16 -0600

    Added app routing

--- Frontend/src/AppRouter/AppRouter.js
+++ Frontend/src/AppRouter/AppRouter.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
 import { Dashboard } from '../Dashboard/Dashboard';
+import { DashboardLoggedIn } from '../DashboardLoggedIn/DashboardLoggedIn';
+import { FuelQuoteForm } from '../FuelQuoteForm/FuelQuoteForm';
+import { FuelQuoteHistory } from '../FuelQuoteHistory/FuelQuoteHistory';
+import { UserAccount } from '../UserAccount/UserAccount';
+import { UserRegistrationFirst } from '../UserRegistrationFirst/UserRegistrationFirst';
+import { UserRegistrationSecond } from '../UserRegistrationSecond/UserRegistrationSecond';
 
 export const AppRouter = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Dashboard />}></Route>
+                <Route path="/DashboardLoggedIn" element={<DashboardLoggedIn />}></Route>
+                <Route path="/FuelQuoteForm" element={<FuelQuoteForm />}></Route>
+                <Route path="/FuelQuoteHistory" element={<FuelQuoteHistory />}></Route>
+                <Route path="/UserAccount" element={<UserAccount />}></Route>
+                <Route path="/UserRegistrationFirst" element={<UserRegistrationFirst />}></Route>
+                <Route path="/UserRegistrationSecond" element={<UserRegistrationSecond />}></Route>
             </Routes>
         </BrowserRouter>
     );
