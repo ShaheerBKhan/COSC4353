98a012b0bcfef6eda53a617d847340e6dc3662b7
Shaheer Khan <K.ShaheerB@gmail.com>
2022-02-15 19:44:59 -0600

    Login button

--- Frontend/src/Dashboard/Dashboard.js
+++ Frontend/src/Dashboard/Dashboard.js
@@ -1,14 +1,26 @@
 import '../index.css';
 import './Dashboard.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Dashboard = () => {
+    const [isLoginFolded, setIsLoginFolded] = useState(true);
+
+    const HandleLogin = () => {
+        setIsLoginFolded(prev => !prev);
+    };
+
     return(
         <div className='container'>
             <div className='dashboard-container'>
                 <div>Image</div>
                 <div>Description</div>
-                <div>Login</div>
+                <div className='login-container'>
+                    <div className='header' onClick={() => HandleLogin()}>Login</div>
+                    <div className='content' style={{display: (isLoginFolded ? 'none' : 'block')}}>
+                        <div>User</div>
+                        <div>Login</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
