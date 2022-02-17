f34af59539e7583eb13893accfe51c7f194ea25e
ShaheerBKhan <K.ShaheerB@gmail.com>
2022-02-16 23:28:12 -0600

    Spacing

--- Frontend/src/Pages/UserRegistrationFirst.js
+++ Frontend/src/Pages/UserRegistrationFirst.js
@@ -1,16 +1,17 @@
 import '../index.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const UserRegistrationFirst = () => {
     return(
         <div className="Custom_Form">
             <div class = "Input_form">
                 <form>
-                    <label for = "Username" > Username </label>
-                    <input type="text" name = "Username" placeholder="Username"></input>
-                    <label for = "Password" > Password</label>
-                    <input type="password" name = "password" placeholder="password"></input>
-                    <input type = "submit" value = "Submit"></input>
+                    <label for="Username" >Username </label>
+                    <input type="text" name="Username" placeholder="Username"></input>
+                    <label for="Password">Password</label>
+                    <input type="password" name="password" placeholder="password"></input>
+                    <input type="submit" value="Submit"></input>
                 </form>
             </div>
         </div>
