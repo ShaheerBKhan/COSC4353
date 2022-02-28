52a51bcba114ceeb09263246c1661a80b57d984d
Shaheer Khan <K.ShaheerB@gmail.com>
2022-02-28 12:07:23 -0600

    Renamed controllers

--- Frontend/src/Pages/Dashboard.js
+++ Frontend/src/Pages/Dashboard.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Image } from './Components/Image';
 
-import { GetUser } from '../API/Login';
+import { GetUser } from '../Controllers/Login';
 
 export const Dashboard = () => {
     const [isLoginFolded, setIsLoginFolded] = useState(true);
