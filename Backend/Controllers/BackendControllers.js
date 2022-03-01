1c7843097022dc1078f1c2015a4f3af12cddc610
ShaheerBKhan <K.ShaheerB@gmail.com>
2022-03-01 00:21:20 -0600

    Post requests working

--- Backend/Controllers/BackendControllers.js
+++ Backend/Controllers/BackendControllers.js
@@ -10,21 +10,18 @@ var client = await DatabaseConnection();
 
 /* *** GET REQUESTS *** */
 app.get('/GetUserInDatabase', async (req, res) => {
-    console.log("Parameters: ", req.query);
     const parameters = req.query;
 
     res.send(true);
 });
 
 app.get('/GetUserAccountInformation', async (req, res) => {
-    console.log("Parameters: ", req.query);
     const parameters = req.query;
 
     res.send(true);
 });
 
 app.get('/GetUserFuelHistory', async (req, res) => {
-    console.log("Parameters: ", req.query);
     const parameters = req.query;
 
     res.send(true);
@@ -33,27 +30,34 @@ app.get('/GetUserFuelHistory', async (req, res) => {
 /* *** POST REQUESTS *** */
 app.post('/PostFuelQuoteForm', async (req, res) => {
     const parameters = req.query;
-    client.query(
+    
+    let dateString = parameters.deliveryDate.toString();
+    dateString = dateString.replace('-','');
+    dateString = dateString.replace('-','');
+
+    const response = await client.query(
         `INSERT INTO FuelQuote(UserId, Gallons, DeliveryAddress, DeliveryDate, PricePerGallon, TotalAmount)
-        VALUES(${parameters.userId}, ${parameters.gallons}, ${parameters.deliveryAddress}, ${parameters.deliveryDate}, ${parameters.pricePerGallon}, ${parameters.totalAmount})`
+        VALUES(${parameters.userId}, ${parameters.gallons}, '${parameters.deliveryAddress}', to_date(${dateString}::text, 'YYYYMMDD'), ${parameters.pricePerGallon}, ${parameters.totalAmount})`
     );
+    console.log(response);
 });
 
 app.post('/PostUserRegistrationFirst', async (req, res) => {
     const parameters = req.query;
-    client.query(
+    const response = await client.query(
         `INSERT INTO Credential(Username, Password)
         VALUES('${parameters.username}', '${parameters.password}')`
     );
+    console.log(response);
 });
 
 app.post('/PostUserRegistrationSecond', async (req, res) => {
-    console.log("Parameters: ", req.query);
     const parameters = req.query;
-    client.query(
+    const response = await client.query(
         `INSERT INTO UserInformation(UserId, Fullname, AddressOne, AddressTwo, City, State, ZipCode)
-        VALUES(${parameters.userId},${parameters.fullName},${parameters.addressOne},${parameters.addressTwo},${parameters.city},${parameters.state},${parameters.zipcode},)`
+        VALUES(${parameters.userId},'${parameters.fullName}','${parameters.addressOne}','${parameters.addressTwo}','${parameters.city}','${parameters.state}',${parameters.zipcode})`
     );
+    console.log(response);
 });
 
 
